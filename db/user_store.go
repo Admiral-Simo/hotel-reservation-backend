d88aa4c7b2b359db83a20a46b45727b7a05b95fe
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-21 18:25:14 +0100

    make handlget get user by id

---
+++ db/user_store.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"context"
+
+	"github.com/Admiral-Simo/HotelReserver/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const (
+	userColl = "users"
+)
+
+type UserStore interface {
+	GetUserById(context.Context, string) (*types.User, error)
+}
+
+type MongoUserStore struct {
+	client *mongo.Client
+	dbname string
+	coll   *mongo.Collection
+}
+
+func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
+	return &MongoUserStore{
+		client: client,
+		coll:   client.Database(DBNAME).Collection(userColl),
+	}
+}
+
+func (s *MongoUserStore) GetUserById(ctx context.Context, id string) (*types.User, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
