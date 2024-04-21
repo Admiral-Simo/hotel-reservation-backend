63cd930a38a8f82ab716ff63992d19e7d5ad5f1e
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-21 20:58:23 +0100

    feat: email and password validation

--- types/user.go
+++ types/user.go
@@ -1,12 +1,19 @@
 package types
 
 import (
+	"errors"
+	"fmt"
+	"regexp"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
 
 const (
-	bcryptCost = 12
+	bcryptCost      = 12
+	minFirstNameLen = 2
+	minLastNameLen  = 2
+	minPasswordLen  = 8
 )
 
 type CreateUserParams struct {
@@ -14,6 +21,27 @@ type CreateUserParams struct {
 	LastName  string `json:"lastName"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
+}
+
+func (params *CreateUserParams) Valide() error {
+	if len(params.FirstName) < minFirstNameLen {
+		return fmt.Errorf("firstName length should be at least %d characters", minFirstNameLen)
+	}
+	if len(params.LastName) < minLastNameLen {
+		return fmt.Errorf("lastName length should be at least %d characters", minLastNameLen)
+	}
+	if len(params.Password) < minPasswordLen {
+		return fmt.Errorf("password length should be at least %d characters", minPasswordLen)
+	}
+	if !isEmailValid(params.Email) {
+		return errors.New("email is invalid")
+	}
+	return nil
+}
+
+func isEmailValid(e string) bool {
+	emailRegex := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`)
+	return emailRegex.MatchString(e)
 }
 
 type User struct {
@@ -25,6 +53,10 @@ type User struct {
 }
 
 func NewUserFromParams(params CreateUserParams) (*User, error) {
+	err := params.Valide()
+	if err != nil {
+		return nil, err
+	}
 	encpw, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcryptCost)
 	if err != nil {
 		return nil, err
