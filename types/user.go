bf4dfdd9e2a82255b8f3dbf326b5e7da000be54c
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-21 19:53:13 +0100

    add user params and create user from params

--- types/user.go
+++ types/user.go
@@ -1,7 +1,35 @@
 package types
 
+import "golang.org/x/crypto/bcrypt"
+
+const (
+	bcryptCost = 12
+)
+
+type CreateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
 type User struct {
-	ID        string `bson:"_id,omitempty" json:"id,omitempty"`
-	FirstName string `bson:"firstName" json:"firstName"`
-	LastName  string `bson:"lastName" json:"lastName"`
+	ID                string `bson:"_id,omitempty" json:"id,omitempty"`
+	FirstName         string `bson:"firstName" json:"firstName"`
+	LastName          string `bson:"lastName" json:"lastName"`
+	Email             string `bson:"email" json:"email"`
+	EncryptedPassword string `bson:"encryptedPassword" json:"-"`
+}
+
+func NewUserFromParams(params CreateUserParams) (*User, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcryptCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		FirstName:         params.FirstName,
+		LastName:          params.LastName,
+		Email:             params.Email,
+		EncryptedPassword: string(encpw),
+	}, nil
 }
