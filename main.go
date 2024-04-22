7cb75c7b37e75302644e7f14fc261da216cb7c7b
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-22 16:46:26 +0100

    add dbname to the params to NewMongoUserStore

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ import (
 )
 
 const (
-	dburi = "mongodb://localhost:27017"
+	dburi  = "mongodb://localhost:27017"
+	dbname = "hotel-reservation"
 )
 
 var config = fiber.Config{
@@ -34,7 +35,7 @@ func main() {
 	}
 
 	// handlers initialization
-	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client, dbname))
 
 	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
