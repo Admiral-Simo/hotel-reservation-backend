99d92a42d678adf6197778cf66cc9a3356b3246f
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-22 14:53:40 +0100

    make UpdateUserParams struct

--- types/user.go
+++ types/user.go
@@ -15,6 +15,11 @@ const (
 	minPasswordLen  = 8
 )
 
+type UpdateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
 type CreateUserParams struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
