03759a4c0821720469f72244aa1acc6871c2cf27
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-24 20:16:06 +0100

    room type -> string, seaside -> bool

--- types/room.go
+++ types/room.go
@@ -2,20 +2,11 @@ package types
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type RoomType int
-
-const (
-	_ RoomType = iota
-	SingleRoomType
-	DoubleRoomType
-	SeaSideRoomType
-	DeluxRoomType
-)
-
 type Room struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Type      RoomType           `bson:"type" json:"type"`
+	Type      string             `bson:"type" json:"type"`
 	BasePrice float64            `bson:"basePrice" json:"basePrice"`
+	SeaSide   bool               `bson:"seaSide" json:"seaSide"`
 	Price     float64            `bson:"price" json:"price"`
 	HotelID   primitive.ObjectID `bson:"hotelID" json:"hotelID"`
 }
