655a749ccb808dfe666e074a2a261e7ceaf5ddcd
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-24 22:45:18 +0100

    naming: remove BasePrice | rename Type to Size

--- types/room.go
+++ types/room.go
@@ -4,8 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Room struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Type      string             `bson:"type" json:"type"`
-	BasePrice float64            `bson:"basePrice" json:"basePrice"`
+	Size      string             `bson:"size" json:"size"`
 	SeaSide   bool               `bson:"seaSide" json:"seaSide"`
 	Price     float64            `bson:"price" json:"price"`
 	HotelID   primitive.ObjectID `bson:"hotelID" json:"hotelID"`
