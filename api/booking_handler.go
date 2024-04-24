6fe5ce4b8858b07029bf786831d77634778a8316
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-24 22:47:23 +0100

    feat: booking handler get bookings | get booking by id

---
+++ api/booking_handler.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Admiral-Simo/HotelReserver/db"
+	"github.com/Admiral-Simo/HotelReserver/types"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type BookingHandler struct {
+	store *db.Store
+}
+
+func NewBookingHandler(store *db.Store) *BookingHandler {
+	return &BookingHandler{
+		store: store,
+	}
+}
+
+// TODO: this needs to be admin authorized!
+func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
+	bookings, err := h.store.Booking.GetBookings(c.Context(), bson.M{})
+	if err != nil {
+		return err
+	}
+	if bookings == nil {
+		return c.JSON([]types.Booking{})
+	}
+	return c.JSON(bookings)
+}
+
+// TODO: this needs to be user authorized
+func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
+	id := c.Params("id")
+	booking, err := h.store.Booking.GetBookingById(c.Context(), id)
+	if err != nil {
+		return err
+	}
+	user, ok := c.Context().UserValue("user").(*types.User)
+	if !ok {
+		return fmt.Errorf("Internal server error")
+	}
+	if booking.UserID != user.ID && !user.IsAdmin {
+		return c.Status(http.StatusUnauthorized).JSON(genericResponse{
+			Type: "error",
+			Msg:  "unauthorized",
+		})
+	}
+	return c.JSON(booking)
+}
