574e06dbd5df2810e5651f296a32567e778bf6ee
Admiral-Simo <admiralsimo16@gmail.com>
2024-05-01 23:09:07 +0100

    refactor(booking handler): remove unnecessary imports

--- api/booking_handler.go
+++ api/booking_handler.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"github.com/Admiral-Simo/HotelReserver/db"
-	"github.com/Admiral-Simo/HotelReserver/types"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
